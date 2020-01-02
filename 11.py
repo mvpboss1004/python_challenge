41a6187e111b715b6bff270f8b0e3e34346fbb5a
timmyhuang <timmyhuang@tencent.com>
2020-01-02 14:45:26 +0800

    11 passed

---
+++ 11.py
@@ -0,0 +1,16 @@
+from PIL import Image,ImageDraw
+import numpy as np
+back = np.array(Image.open('cave.jpg'))
+odd = back.copy()
+even = back.copy()
+for i in range(back.shape[0]):
+    for j in range(back.shape[1]):
+        if (i+j)%2 == 0:
+            for k in range(3):
+                even[i][j][k] = 0
+        else:
+            for k in range(3):
+                odd[i][j][k] = 0
+Image.fromarray(odd).show()
+Image.fromarray(even).show()
+print('evil')
